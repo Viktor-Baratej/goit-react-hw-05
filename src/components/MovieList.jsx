74bf8979ca17faded06a8ec0feaab5861ac18839
Viktor_Baratejj <Viktor_baratejj@ukr.net>
2025-01-08 12:24:46 +0200

    fixed

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import s from './MovieList.module.css';
 
 function MovieList({ movies }) {
+  const location = useLocation();
   return (
     <ul className={s.list}>
       {movies.map(movie => (
         <li key={movie.id} className={s.item}>
-          <Link to={`/movies/${movie.id}`}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-            />
-            <p>{movie.title}</p>
+          <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+            {movie.title || movie.name}
           </Link>
         </li>
       ))}
